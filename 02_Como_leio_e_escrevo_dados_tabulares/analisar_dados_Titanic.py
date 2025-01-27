5217033c06ef51f9d13c3f047c6576b56250aa32
Matheus <matheusparreira.cruz@gmail.com>
2025-01-27 16:58:57 -0300

    Novo

--- 02_Como_leio_e_escrevo_dados_tabulares/analisar_dados_Titanic.py
+++ 02_Como_leio_e_escrevo_dados_tabulares/analisar_dados_Titanic.py
@@ -1,4 +1,14 @@
 import pandas as pd
-titanic = pd.read_csv("titanic.csv")
+titanic = pd.read_csv("Titanic.csv")
 
 print(titanic)
+
+print(titanic.head(8))# Quero ver as primeiras 8 linhas de um DataFrame do pandas.
+
+print(titanic.tail(8))# Quero ver as últimas 8 linhas de um DataFrame do pandas.
+
+print(titanic.dtypes)# Os tipos de dado aqui DataFrame são inteiros ( int64), flutuantes ( float64) e strings ( object).
+
+titanic.to_excel("Titanic.xlsx", sheet_name="passengers", index=False)
+
+print(titanic.info())
